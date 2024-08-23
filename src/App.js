20a94032190b1ffc460c1ebdd6ba2254a976c82b
olakunle <devlakunle@gmail.com>
2024-08-23 03:57:47 +0100

    Configured routes for login, signup, contacts, and contact details with protected routes using AuthProvider and ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ContactsPage from "./pages/ContactsPage";
+import ContactDetailPage from "./pages/ContactDetailPage";
+import EditContactPage from "./pages/EditContactPage";
+import CreateContactPage from "./pages/CreateContactPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import HomePage from "./pages/Homepage";
+import AuthProvider from "./authContext";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/contacts" element={<ContactsPage />} />
+            <Route path="/contacts/create" element={<CreateContactPage />} />
+            <Route path="/contacts/:id" element={<ContactDetailPage />} />
+            <Route path="/contacts/edit/:id" element={<EditContactPage />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
